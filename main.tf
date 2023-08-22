f411bb09cfef9ed4dab71465dd315a704e6558d8
Sharnell Grant <sharnellgrant@gmail.com>
2023-08-22 01:04:39 +0000

    created provider and ec2 code

--- main.tf
+++ main.tf
@@ -1,1 +1,24 @@
 #terraform file 
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web_server" {                            # BLOCK
+  ami           = data.aws_ami.ubuntu.id                          # Argument with data expression
+  instance_type = "t2.micro"                                      # Argument
+  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id # Argument with value as expression
+  tags = {
+    Name = "Web EC2 Server"
+  }
+}
+
