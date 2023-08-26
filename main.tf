752e9455f9f3c96d98385ef4e9514bbd3dbc5b0f
Sharnell Grant <sharnellgrant@gmail.com>
2023-08-26 18:24:16 +0000

    made changes to the inbound rules

--- main.tf
+++ main.tf
@@ -13,14 +13,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "jenkins_web_server" {                            
-  ami           = "ami-08a52ddb321b32a8c"                          
-  instance_type = "t2.micro"                                      
-  
+resource "aws_instance" "jenkins_web_server" {
+  ami           = "ami-08a52ddb321b32a8c"
+  instance_type = "t2.micro"
+
   tags = {
     Name = "Jenkins Web Server"
   }
-  
+
   user_data = <<-EOF
     #!/bin/bash
     sudo yum update -y 
@@ -29,7 +29,7 @@ resource "aws_instance" "jenkins_web_server" {
     sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
     sudo yum upgrade -y
     sudo amazon-linux-extras install java-openjdk11 -y
-    yum install jenkins -y
+    sudo yum install jenkins -y
     systemctl enable jenkins
     systemctl start jenkins
     EOF
@@ -41,24 +41,37 @@ resource "aws_security_group" "jenkins_web_server_security_group" {
   vpc_id      = "vpc-0b47b6adeff3753ef"
 
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   
   ingress {
-    from_port        = 8080
-    to_port          = 8080
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   
+  engress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-resource "aws_s3_bucket" "my_jenkins_s3_bucket" {
-  bucket = "my_jenkins_s3_bucket"
+resource "aws_s3_bucket" "myjenkinss3bucket" {
+  bucket = "myjenkinss3bucket"
 }
 
